63bee5551704f0d209a3a9732ff312604edaad3c
Navinda Lankesh <47824016+paradocx96@users.noreply.github.com>
2020-12-14 14:32:38 +0530

    Add Login Testing Methods

--- src/test/java/com/paradocx96/hrm/TestLogin.java
+++ src/test/java/com/paradocx96/hrm/TestLogin.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -14,13 +15,27 @@ public class TestLogin {
 
     private WebDriver webDriver;
 
+    @BeforeClass
+    public void beforeClass() {
+        WebDriverManager.chromedriver().setup();
+    }
+
     @BeforeMethod
     private WebDriver beforeMethod() {
-        WebDriverManager.chromedriver().setup();
         webDriver = new ChromeDriver();
         webDriver.get("http://hrm.pragmatictestlabs.com");
         return webDriver;
     }
+
+    @AfterMethod
+    private void afterMethod() {
+        //Close the browser
+        webDriver.close();
+    }
+
+    /*
+     *Testing valid user login
+     */
 
     @Test
     public void testValidUserLogin() {
@@ -36,7 +51,7 @@ public class TestLogin {
 
         //Verify welcome message
         String welcome = webDriver.findElement(By.id("welcome")).getText();
-        Assert.assertEquals(welcome,"Welcome Admin");
+        Assert.assertEquals(welcome, "Welcome Admin");
     }
 
     @Test
@@ -50,7 +65,7 @@ public class TestLogin {
 
         //Verify welcome message
         String welcome = webDriver.findElement(By.id("welcome")).getText();
-        Assert.assertEquals(welcome,"Welcome Admin");
+        Assert.assertEquals(welcome, "Welcome Admin");
     }
 
     @Test
@@ -59,18 +74,106 @@ public class TestLogin {
         //Type username
         webDriver.findElement(By.id("txtUsername")).sendKeys("Admin");
 
-        //Type password and Press Enter key
+        //Type password and Call Submit Method
         webDriver.findElement(By.id("txtPassword")).sendKeys("Ptl@#321");
         webDriver.findElement(By.id("txtPassword")).submit();
 
         //Verify welcome message
         String welcome = webDriver.findElement(By.id("welcome")).getText();
-        Assert.assertEquals(welcome,"Welcome Admin");
+        Assert.assertEquals(welcome, "Welcome Admin");
     }
 
-    @AfterMethod
-    private void afterMethod() {
-        //Close the browser
-        webDriver.close();
+    /*
+     *Testing Invalid user login
+     */
+
+    @Test
+    public void testUserLoginWithEmptyUsernamePassword() {
+
+        //Type username
+        webDriver.findElement(By.id("txtUsername")).clear();
+
+        //Type password and Call Submit Method
+        webDriver.findElement(By.id("txtPassword")).clear();
+        webDriver.findElement(By.id("txtPassword")).submit();
+
+        //Verify message
+        String message = webDriver.findElement(By.id("spanMessage")).getText();
+        Assert.assertEquals(message, "Username cannot be empty");
+    }
+
+    @Test
+    public void testUserLoginWithEmptyPassword() {
+
+        //Type username
+        webDriver.findElement(By.id("txtUsername")).sendKeys("Admin");
+
+        //Type password and Call Submit Method
+        webDriver.findElement(By.id("txtPassword")).clear();
+        webDriver.findElement(By.id("txtPassword")).submit();
+
+        //Verify message
+        String message = webDriver.findElement(By.id("spanMessage")).getText();
+        Assert.assertEquals(message, "Password cannot be empty");
+    }
+
+    @Test
+    public void testUserLoginWithEmptyUsername() {
+
+        //Type username
+        webDriver.findElement(By.id("txtUsername")).clear();
+
+        //Type password and Call Submit Method
+        webDriver.findElement(By.id("txtPassword")).sendKeys("Ptl@#321");
+        webDriver.findElement(By.id("txtPassword")).submit();
+
+        //Verify message
+        String message = webDriver.findElement(By.id("spanMessage")).getText();
+        Assert.assertEquals(message, "Username cannot be empty");
+    }
+
+    @Test
+    public void testUserLoginWithWrongUsername() {
+
+        //Type username
+        webDriver.findElement(By.id("txtUsername")).sendKeys("Customer");
+
+        //Type password and Call Submit Method
+        webDriver.findElement(By.id("txtPassword")).sendKeys("Ptl@#321");
+        webDriver.findElement(By.id("txtPassword")).submit();
+
+        //Verify message
+        String message = webDriver.findElement(By.id("spanMessage")).getText();
+        Assert.assertEquals(message, "Invalid credentials");
+    }
+
+    @Test
+    public void testUserLoginWithWrongPassword() {
+
+        //Type username
+        webDriver.findElement(By.id("txtUsername")).sendKeys("Admin");
+
+        //Type password and Call Submit Method
+        webDriver.findElement(By.id("txtPassword")).sendKeys("Password");
+        webDriver.findElement(By.id("txtPassword")).submit();
+
+        //Verify message
+        String message = webDriver.findElement(By.id("spanMessage")).getText();
+        Assert.assertEquals(message, "Invalid credentials");
+    }
+
+    @Test
+    public void testUserLoginWithCaseSensitivityPassword() {
+
+        //Type username
+        webDriver.findElement(By.id("txtUsername")).sendKeys("Admin");
+
+        //Type password and Call Submit Method
+        webDriver.findElement(By.id("txtPassword")).sendKeys("pTL@#321");
+        webDriver.findElement(By.id("txtPassword")).submit();
+
+        //Verify message
+        String message = webDriver.findElement(By.id("spanMessage")).getText();
+        Assert.assertEquals(message, "Invalid credentials");
     }
 }
